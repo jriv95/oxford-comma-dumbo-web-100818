9e484ad5b562995971f8bf0cef7b7eed5a48c198
Joel Rivera <joel.rivera7995@gmail.com>
2018-09-30 22:59:07 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,6 +3,8 @@ def oxford_comma(array)
   array.join
 elsif array.length == 2
   array.insert(-2, "and").join(" ")
+elsif array.length == 3
+  array.insert(-2, "and").join(", ")
 else array.insert(-2, "and").join(", ")
 end
 end
