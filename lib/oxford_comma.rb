f875636c5853d630eb83a62e922265027cd43a1b
Joel Rivera <joel.rivera7995@gmail.com>
2018-09-30 22:14:46 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,6 +3,6 @@ def oxford_comma(array)
   array.join
 elsif array.length == 2
   array.insert(-2, "and").join(" ")
-else array.insert(-2, "and").join(", ")
+else array.insert(-2, "and").to_sentence(:last_word_connector => ' and ')
 end
 end
