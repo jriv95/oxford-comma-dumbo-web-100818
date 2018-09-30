7d32aaa05d63f32ef476bbeac658161da850f8b9
Joel Rivera <joel.rivera7995@gmail.com>
2018-09-30 22:35:14 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,7 @@ def oxford_comma(array)
   if array.length == 1
   array.join
 elsif array.length == 2
-  array.pop.insert(-1, "and starfruit").join(" ")
+  array.insert(-2, "and").join(" ")
 else array.pop.insert(-2, "and dragon fruits").join(", ")
 end
 end
