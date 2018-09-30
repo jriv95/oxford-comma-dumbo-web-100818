8715869d12aa89799396f5b60ee92071be055545
Joel Rivera <joel.rivera7995@gmail.com>
2018-09-30 22:07:13 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,9 @@
 def oxford_comma(array)
-
+  if array.length == 1
+  array.join
+elsif array.length == 2
+  array.insert(-2, "and").join(" ")
+else array.insert(-2, "and").join(", ")
+  array.to_sentence(:last_word_connector => " and ")
+end
 end
