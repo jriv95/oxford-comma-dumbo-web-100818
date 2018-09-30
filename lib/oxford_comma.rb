10558f6ccb81603fcc9f74aea64c9cf244e7f1dd
Joel Rivera <joel.rivera7995@gmail.com>
2018-09-30 22:37:31 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,8 +3,6 @@ def oxford_comma(array)
   array.join
 elsif array.length == 2
   array.insert(-2, "and").join(" ")
-elsif array.length == 3
-  array.pop.insert(-1, "and starfruit").#join(", ")
-else array.pop.insert(-1, "and dragon fruits").join(", ")
+else array.insert(-2, "and").join(", ")
 end
 end
